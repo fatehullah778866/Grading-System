dba0ceae4083be516907ef5d15c3004b1494bafc
amjiofficial <amjadkhanofficial124@gmail.com>
2024-07-30 07:58:52 -0700

    Add grading system

---
+++ amjid/main.py
@@ -0,0 +1,14 @@
+student_score =float(input("Enter student score:"))
+if student_score>=90 and student_score<=100:
+    print ("Grade A")
+elif student_score>=80 and student_score<=89:
+    print ("Grade B")
+elif student_score>=70 and student_score<=79:
+    print ("Grade C")
+elif student_score>=60 and student_score<=69:
+    print ("Grade E")
+else:
+    print("Fail")
+
+
+
